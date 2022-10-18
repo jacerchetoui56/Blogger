a42b4fe0b9ecf919c8cf5c4a22f1e0dc22140522
jacerchetoui56 <jacerdjo@gmail.com>
2022-10-18 17:19:43 +0100

    finished with the project

--- src/controllers/articleController.ts
+++ src/controllers/articleController.ts
@@ -1,36 +1,76 @@
+import { marked } from "marked";
+import slugify from "slugify";
+import { JSDOM } from "jsdom";
+import createDomPurify from "dompurify";
+
+const dompurify = createDomPurify((new JSDOM() as any).window);
+
 import { Response, Request } from "express";
 import Article from "../models/article.model";
 
 const addArticle = async (req: Request, res: Response) => {
   const { title, description, markdown } = req.body;
-  if(!title || !description || !markdown ){
-    return res.render('articles/new', {article : {title , description, markdown}})
+  if (!title || !description || !markdown) {
+    return res.render("articles/new", {
+      article: { title, description, markdown },
+    });
   }
   const article = await Article.create({ title, description, markdown });
-    res.redirect(`/articles/${article.slug}`);
+  res.redirect(`/articles/${article.slug}`);
 };
-
 
 const getArticle = async (req: Request, res: Response) => {
   const { slug } = req.params;
   try {
-    const article = await Article.findOne({slug : slug});
-    if(!article) return res.redirect('/')
-    res.render(`articles/show`, {article : article});
-  }catch(err){
-    res.redirect('/')
+    const article = await Article.findOne({ slug: slug });
+    if (!article) return res.redirect("/");
+    res.render(`articles/show`, { article: article });
+  } catch (err) {
+    res.redirect("/");
   }
 };
 
+const deleteArticle = async (req: Request, res: Response) => {
+  const { id } = req.params;
+  try {
+    const article = await Article.findByIdAndDelete(id);
+    res.redirect("/");
+  } catch (err) {
+    console.log(err);
+  }
+};
 
-const deleteArticle = async (req : Request, res : Response)=> {
-    const {id } = req.params
-    try {
-      const article = await Article.findByIdAndDelete(id)
-      res.redirect('/')
-    }catch(err){
-      console.log(err);
-    }
-}
+const getArticleToEdit = async (req: Request, res: Response) => {
+  const { id } = req.params;
+  try {
+    const article = await Article.findById(id).select({
+      title: 1,
+      description: 1,
+      markdown: 1,
+    });
+    if (!article) res.redirect("/");
+    res.render("articles/edit", { article: article });
+  } catch (error) {
+    res.redirect("/");
+  }
+};
 
-export {deleteArticle, addArticle, getArticle };
+const editArticle = async (req: Request, res: Response) => {
+  const { id } = req.params;
+  const { title, description, markdown } = req.body;
+    let slug : string = slugify(title, { lower: true, strict: true });
+    let sanitzedHTML :string = dompurify.sanitize(marked(markdown));
+  try {
+    const article = await Article.findByIdAndUpdate(
+      id,
+      { title, description, markdown, sanitzedHTML , slug},
+      { new: true, runValidators: true }
+    );
+    if (!article) res.redirect("/");
+    res.redirect(`/articles/${article?.slug}`);
+  } catch (error) {
+    res.redirect("/");
+  }
+};
+
+export { deleteArticle, getArticleToEdit, addArticle, getArticle, editArticle };
--- src/models/article.model.ts
+++ src/models/article.model.ts
@@ -1,51 +1,49 @@
 import mongoose from "mongoose";
-import {marked} from 'marked'
+import { marked } from "marked";
 import slugify from "slugify";
-import {JSDOM} from 'jsdom'
-import createDomPurify from 'dompurify'
+import { JSDOM } from "jsdom";
+import createDomPurify from "dompurify";
 
-const dompurify = createDomPurify((new JSDOM() as any).window)
+const dompurify = createDomPurify((new JSDOM() as any).window);
 
 const articleSchema = new mongoose.Schema({
-    title : {
-        type : String, 
-        required : [true , 'Please provide the title'],
-        maxlength : 50
-    },
-    createdAt :  {
-        type : Date,
-        default : Date.now
-    },
-    description : {
-        type : String, 
-        required : [true, 'Please provide a description']
-    },
-    markdown  :{
-        type : String, 
-        require: [true , 'Please provide the markdown']
-    }, 
-    slug : {
-        type : String, 
-        required:  true, 
-        unique : true 
-    }, 
-    sanitzedHTML : {
-        type : String,
-        required: true
-    }
-})
+  title: {
+    type: String,
+    required: [true, "Please provide the title"],
+    maxlength: 50,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+  description: {
+    type: String,
+    required: [true, "Please provide a description"],
+  },
+  markdown: {
+    type: String,
+    require: [true, "Please provide the markdown"],
+  },
+  slug: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  sanitzedHTML: {
+    type: String,
+    required: true,
+  },
+});
 
-articleSchema.pre('validate', function(next){
-    if(this.title) {
-        this.slug = slugify(this.title, {lower : true , strict: true})
-    }
+articleSchema.pre("validate", function () {
+  console.log(this);
+  if (this.title) {
+    this.slug = slugify(this.title, { lower: true, strict: true });
+  }
 
-    if(this.markdown){
-        this.sanitzedHTML = dompurify.sanitize(marked(this.markdown))
-    }
-    next()
-})
+  if (this.markdown) {
+    this.sanitzedHTML = dompurify.sanitize(marked(this.markdown));
+  }
+});
 
-
-
-export default mongoose.model('articles', articleSchema)
+export default mongoose.model("articles", articleSchema);
